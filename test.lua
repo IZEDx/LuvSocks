891c550002e6efe43a47abf17b67d5fe1470e52d
Niklas Kühtmann <nk@baltray.org>
2015-10-15 14:58:36 +0200

    Updated test suite to be easier understandable.

--- test.lua
+++ test.lua
@@ -7,13 +7,8 @@ print("LuvSocks server running on port 1337")
 server:on("connect", function(client)
 	client:on("pong", function(data)
 		print("Pong received:", data)
-		if type(data) == "table" then
-			for k,v in pairs(data) do
-				print("",k,v)
-			end
-		end
 	end)
 
-	client:send("ping")
-	print("Ping sent.")
+	client:send("ping", "Hello there!")
+	print("Ping sent: Hello there!")
 end)
