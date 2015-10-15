cbfe24d85efaf991346cd2bd581811631640068d
Niklas Kühtmann <nk@baltray.org>
2015-10-15 12:18:54 +0200

    Added test suite.

---
+++ test.lua
@@ -0,0 +1,14 @@
+local LuvSocks = require("../LuvSocks")
+
+local server = LuvSocks.new():listen(1337)
+print("LuvSocks server running on port 1337")
+
+
+server:on("connect", function(client)
+	client:on("pong", function(data)
+		print("Pong received:", data)
+	end)
+
+	client:send("ping")
+	print("Ping sent.")
+end)
